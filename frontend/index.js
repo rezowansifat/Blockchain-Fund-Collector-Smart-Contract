6f76ff7282107e298758d88d500da3bd5599256c
Rezowan Sifat <navidrezowansifat@gmail.com>
2023-04-02 00:45:51 +0600

    connecting metamusk to website

---
+++ frontend/index.js
@@ -0,0 +1,17 @@
+const connectButton = document.getElementById("connectbutton")
+
+async function connect() {
+    if (typeof window.ethereum !== "undefined") {
+      try {
+        await ethereum.request({ method: "eth_requestAccounts" })
+      } catch (error) {
+        console.log(error)
+      }
+      connectButton.innerHTML = "Connected"
+      const accounts = await ethereum.request({ method: "eth_accounts" })
+      console.log(accounts)
+    } else {
+      connectButton.innerHTML = "Please install MetaMask"
+    }
+  }
+  
