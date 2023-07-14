21e96be2c2f1db126cfccc20bd98fa1a34155e64
Rezowan Sifat <navidrezowansifat@gmail.com>
2023-07-14 15:45:40 +0600

    Connecting metamask to frontend

--- frontend/index.js
+++ frontend/index.js
@@ -1,36 +1,61 @@
-import { ethers } from "./ethers-5.6.esm.min.js"
-import { abi } from "./constants.js"
+import { ethers } from "./ethers-5.6.esm.min.js";
+import { abi, contractAddress } from "./constants.js";
 
-const connectButton = document.getElementById("connectbutton")
-const fundButton = document.getElementById("fundbutton")
+const connectButton = document.getElementById("connectbutton");
+const fundButton = document.getElementById("fundbutton");
 
-connectButton.onclick = connect
-fundButton.onclick = fund
+connectButton.onclick = connect;
+fundButton.onclick = fund;
 
 console.log(ethers);
 
 async function connect() {
-    if (typeof window.ethereum !== "undefined") {
-      try {
-        await ethereum.request({ method: "eth_requestAccounts" })
-      } catch (error) {
-        console.log(error)
-      }
-      connectButton.innerHTML = "Connected"
-      const accounts = await ethereum.request({ method: "eth_accounts" })
-      console.log(accounts)
-    } else {
-      connectButton.innerHTML = "Please install MetaMask"
+  if (typeof window.ethereum !== "undefined") {
+    try {
+      await ethereum.request({ method: "eth_requestAccounts" });
+    } catch (error) {
+      console.log(error);
+    }
+    connectButton.innerHTML = "Connected";
+    const accounts = await ethereum.request({ method: "eth_accounts" });
+    console.log(accounts);
+  } else {
+    connectButton.innerHTML = "Please install MetaMask";
+  }
+}
+
+async function fund() {
+  const ethAmount = "77";
+  if (typeof window.ethereum !== "undefined") {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(contractAddress, abi, signer);
+
+    try {
+      const trxresponse = await contract.fund({
+        value: ethers.utils.parseEther(ethAmount),
+      });
+
+      await listenForTransactionMine(trxresponse, provider);
+    } catch (error) {
+      console.log(error);
     }
   }
-  
+}
 
-  async function fund(){
-    // console.log(`Funding with ${ethAmount} .....`)
-    if (typeof window.ethereum !== "undefined"){
-      const provider = new ethers.providers.Web3Provider(window.ethereum)
-      const signer = provider.getSigner()
+function listenForTransactionMine(trxresponse, provider) {
+  console.log(`Mining ${trxresponse.hash}........`);
 
-      console.log(signer);
+  return new Promise((resolve, reject) => {
+    try {
+      provider.once(trxresponse.hash, (transactionReceipt) => {
+        console.log(
+          `Completed with ${transactionReceipt.confirmations} confirmations. `
+        );
+        resolve();
+      });
+    } catch (error) {
+      reject(error);
     }
-  }
+  });
+}
